665ed5148c9e6504e9a461e32f69aa6bf0a6121e
BimalMurali <71392855+BimalMurali@users.noreply.github.com>
2020-10-26 10:49:20 +0530

    Add files via upload

---
+++ FactorsOfNumber.java
@@ -0,0 +1,15 @@
+import java.io.*;
+ class FactorsOfNumber
+ {
+   public static void main(String args[])
+   { 
+     int n=10;
+     for(int i=1;i<=n;i++)
+     {
+       if(n%i==0)
+       {
+         System.out.println(i+"  is a factor of  "+n);
+       }
+   }
+ }
+} 
