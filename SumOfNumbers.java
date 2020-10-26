5b571c2d7df267c789458356015b5b4ba400cbd8
BimalMurali <71392855+BimalMurali@users.noreply.github.com>
2020-10-26 10:48:21 +0530

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ class SumOfNumbers
+ {
+   public static void main(String args[])
+   { 
+     int n=10,sum=0;
+     for(int i=1;i<=n;i++)
+     {
+       sum=sum+i;
+     }
+    System.out.println("Sum of the First 'n' number is :"+sum);
+   }
+ }
