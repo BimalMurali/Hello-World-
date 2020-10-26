fb6cfb34ea8128ce562edb0eb931a456fffddaa0
BimalMurali <71392855+BimalMurali@users.noreply.github.com>
2020-10-26 10:46:58 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+ class EvenOrOdd
+ {
+   public static void main(String args[])
+   { 
+     int a=0;
+     if(a%2==0)
+     {
+    System.out.println(" a is even number");
+    }
+    else if(a%2==1)
+     {
+    System.out.println( "a is odd number ");
+    }
+    else 
+    System.out.println("Neither even nor odd");
+   }
+ }
